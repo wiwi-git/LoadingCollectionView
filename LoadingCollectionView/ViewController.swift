e0f30ec8be0860a661a10e6b7a3e00363e239139
wi <duse34@hotmail.com>
2021-02-17 15:31:20 +0900

    Initial Commit

---
+++ LoadingCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoadingCollectionView
+//
+//  Created by 위대연 on 2021/02/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
